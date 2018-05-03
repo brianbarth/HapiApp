229f4bf7f7f30ea5c16848e2c84f726a3ba98cda
brianbarth <bsbarth101@yahoo.com>
2018-05-03 09:41:46 -0500

    making first commit

---
+++ server.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
+const Vision = require('vision');
+const styles = require('./styles.json');
+const dataFile = require('./data.json');
+
+
+
+const server = Hapi.server({
+  port: 4000,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'static')
+    }
+  }
+});
+
+const viewOptions = {layout: 'layout'};
+const start = async () => {
+
+  await server.register(Inert);
+  await server.register(Vision);
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'templates',
+    layoutPath: 'templates/layout',
+    helpersPath: 'templates/helpers'
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, h) {
+      const data = { now: new Date().toString(), firstName: 'Bill', lastName: 'Bankes' };
+      return h.view('index', data, viewOptions)
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/page2.html',
+    handler: function (request, h) {
+      const data = { now: new Date().toString(), firstName: 'Mary', lastName: 'James' };
+      return h.view('page2', styles, viewOptions)
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/page3.html',
+    handler: function (request, h) {
+      const data = { fullName: 'Mary James' };
+      return h.view('page3', data, viewOptions)
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/page4.html',
+    handler: function (request, h) {
+      return h.view('page4', dataFile, viewOptions)
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/page5.html',
+    handler: function (request, h) {
+    
+      return h.view('page5', styles, viewOptions)
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.'
+      }
+    }
+  });
+
+  await server.start();
+
+  console.log('Server started listening on %s', server.info.uri);
+
+};
+
+start();
---
+++ templates/helpers/secretCode.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  
+  const randomCode = Math.floor(100000 + Math.random() * 900000);
+
+  return randomCode;
+};
